7b94d0793b102465b043333b87ca458b08c06a74
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-13 17:16:02 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const account_ID= 55555 // this cant be changed
+let account="hhhhh" // this is mutable and donot have scope prob 
+var pass="123" // this changes it permanently scope problem 
+acity= "palwal"
+let state;
+console.table([account_ID, account, pass, acity, state])
