6b43517afd55eddd4726b27ce07ae805f177e034
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-14 08:51:54 +0000

    comm

---
+++ 01_basics/05_DATATUPE_SUM.js
@@ -0,0 +1,13 @@
+let id1= Symbol(123);
+let id2= Symbol(123);
+
+console.log(id1 === id2); // they both have same value but they are diff due to symbol symbol is used when there are same variable many times
+
+const arr =["har", "shar", "pal"]
+
+console.log(typeof arr);
+
+let obj = { // this for the object 
+    name: "har", 
+    roll: "22"
+}
