e695a749222c29e821a94b2a0d373eb60cfb1441
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-14 09:13:15 +0000

    comm

--- 01_basics/05_DATATUPE_SUM.js
+++ 01_basics/05_DATATUPE_SUM.js
@@ -1,3 +1,4 @@
+/*
 let id1= Symbol(123);
 let id2= Symbol(123);
 
@@ -11,3 +12,29 @@ let obj = { // this for the object
     name: "har", 
     roll: "22"
 }
+    */
+   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack memory (primitive) - this gives the copy of the valye all values which are changed are done on the copy 
+
+// head(non primirive )- give orignal refrence of the orignal values all changes will be done on the orignal value
+
+let a = 123;
+let b= a;
+
+b= 456;
+console.log(a); // here copy id is provided so the we changed in b nothing happened in a
+console.log(b);
+
+
+let user1={
+    name: "hars",
+    age: "22"
+}
+
+let user2= user1; // here direct refrence is provided every changes will be done on the orignal vaue
+user2.name="sharma"; // we are doing changes in user2 name and it is refrence so the chang will be done on the whole object so user1 nme will also be changed 
+
+console.log(user1.name);
+console.log(user2.name);
+
