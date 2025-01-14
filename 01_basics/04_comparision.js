ee24301b0967021f922cdbadaf2c5b9a5b4b5c88
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-14 06:19:45 +0000

    coomm

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+console.log(null== 0);
+console.log(null <=0); // wile comparing null it is converted in 0
+console.log(null >=0);
+console.log(null > 0 );
+
+// === this do comprision of no. and datatype as well 
+console.log(" ");
+
+console.log(2 ===2);
+console.log("2" === 2);
+
+
+
