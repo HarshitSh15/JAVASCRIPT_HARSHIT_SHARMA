6144d4597de915521019122586840225896f69a6
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-14 04:18:38 +0000

    03

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,31 @@
+let age ="33";
+
+console.log(typeof age);
+
+let x = Number(age);
+
+console.log(typeof x);
+
+let k = "22ab";
+console.log(typeof k);
+
+let y = Number(k); // here we are convertinng a string which cant be converted in the number 
+
+console.log(typeof y);// this will tell that k is converted in the number 
+console.log(y); // this will give NaN not a number which means that it cant be converted in the number 
+
+/*
+if we take boolean than in number it will give value 0,1
+
+if we take null than it will give value in 0;
+*/
+let a = null;
+let b = Number(a);
+
+console.log(b);
+
+// undefined is will be converted in NaN
+//  same the variables can be converted in the boolean 
+let m =1;
+let n = Boolean(m);
+console.log(n);
