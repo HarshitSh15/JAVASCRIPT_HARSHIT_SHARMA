d53e142c4153c1e4219b84c9cd5347b5875fa2dd
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-14 03:53:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // this ensure that we use newer version of the js 
+// alert(3) we are using node js not browser 
+// null- stand alone 
+// undefined
+//
+
+let age= 333;
+let name = "har";
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof undefined); // it is type
+console.log(typeof null); // it is an object 
