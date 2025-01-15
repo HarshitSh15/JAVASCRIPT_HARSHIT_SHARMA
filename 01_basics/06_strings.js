e0539e30db0b6c1760c353f5479b0db1e2dacf9a
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-15 06:27:58 +0000

    comm

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,49 @@
+/*
+let name ="har";
+let age =22;
+
+console.log(`my name is ${name} and my ageis ${age}`);
+
+const x ="qqqqqqqqqqqqqqqqqqqqqqqqq";
+const y = new String("absabsabs");
+console.log(x[0]);
+
+console.log(y[4]);
+
+console.log(x.__proto__);
+console.log(y.__proto__);
+*/
+//const x ="harshit shrama";
+/*
+console.log(x.length);
+console.log(x.toUpperCase());
+console.log(x.charAt(2));
+console.log(x.indexOf('t'));
+*/
+/*
+const newstr = x.substring(0,4); // start with 0indexing and end end before 4 so the last index it will cover is 3
+console.log(newstr);
+
+const y = x.slice(-19, 9); // same as substring but allow -ve indexing  
+console.log(y);
+
+
+// note - all changes are done are on the copy of the variable 
+
+*/
+/*
+const a = "        harshit"
+console.log(a);
+
+console.log(a.trim()); // this remove blank spaces 
+
+
+const url="http_23_harshit%20sharma";
+
+console.log(url.replace("20","66")); this will replace 20
+console.log(url.includes("har"));
+*/
+
+const x ="harshit-sharma-palwal"; // this will make array on the basis of the '-' harshit will be on element in array and sharma will b ethe another element    
+
+console.log(x.split('-'));
