17e33dc1282f3ab8a2d76c710e5b6955218cedaa
HarshitSh15 <163673939+HarshitSh15@users.noreply.github.com>
2025-01-20 15:04:19 +0000

    comm

---
+++ 04_control_flow/01_switch.js
@@ -0,0 +1,25 @@
+let x =9;
+
+switch (x) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("mar");
+        
+        break;
+    case 4:
+        console.log("jaapr");
+        
+        break;
+
+    default:
+        console.log("no thing");
+        
+        break;
+}
